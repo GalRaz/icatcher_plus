11270b0209449ad004623b8a117ef6f918f25cca
yot <erelyotam@gmail.com>
2021-09-23 16:55:17 +0300

    adds scikit to requirements

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from pathlib import Path
 classes = {'away': 0, 'left': 1, 'right': 2}
 face_model_file = Path("models", "face_model.caffemodel")
 config_file = Path("models", "config.prototxt")
-project_data_dir = Path('/Users/Peng/LookIt')
+project_data_dir = Path("/disk3/yotam/icatcher+/mit_dataset_raw/")
 raw_folder = project_data_dir / 'LookIt_raw'
 database = project_data_dir
 video_folder = database / 'videos'
