aa619e3df2d7b07c8171d577e45bd92a30881071
yot <erelyotam@gmail.com>
2021-10-27 13:49:10 +0300

    reversed config file commit

--- config.py
+++ config.py
@@ -1,11 +1,19 @@
 from pathlib import Path
-
+#todo: should all move to command line arguments
 classes = {"away": 0, "left": 1, "right": 2}
 face_model_file = Path("models", "face_model.caffemodel")
 config_file = Path("models", "config.prototxt")
-raw_dataset_folder = Path("/home/jupyter/data/lookIt_raw")
-processed_data_set_folder = Path("/home/jupyter/data/lookIt")
-video_folder = raw_dataset_folder / "videos"
+
+# replace these values with your folder structure !
+raw_dataset_folder = Path("datasets", "lookit_raw")
+processed_data_set_folder = Path("datasets", "lookit")
+video_folder = raw_dataset_folder / "raw_videos"
+
+# using jupyter
+# raw_dataset_folder = Path("/home/jupyter/data/lookIt_raw")
+# processed_data_set_folder = Path("/home/jupyter/data/lookIt")
+# video_folder = raw_dataset_folder / "videos"
+
 faces_folder = processed_data_set_folder / "faces"
 label_folder = processed_data_set_folder / "coding_first"
 label2_folder = processed_data_set_folder / "coding_second"
